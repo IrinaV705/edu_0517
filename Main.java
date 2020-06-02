ec13edbab5da96ab2cb1c960d56528774a6e0b65
IrinaV705 <66119487+IrinaV705@users.noreply.github.com>
2020-06-02 14:59:42 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String name= "Ирина";
+        int age= 25;
+        String city = "Moscow";
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(city);
+    }
+}
