e38c848c422710a670a040928e78884ee8cfee86
IrinaV705 <66119487+IrinaV705@users.noreply.github.com>
2020-06-02 16:24:07 +0300

    Create task_1.6.java

---
+++ task_1.6.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        int a,b;
+        System.out.print("Число: ");
+        a = scanner.nextInt();
+        System.out.println(a*1.15);
+
+
+    }
+}
+/*
+Пользователь вводит число, необходимо увеличить это число на 15% и вывести результат на экран
+*/
