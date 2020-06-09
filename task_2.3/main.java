0e9f713ceb8a73d09cf13303f4336daddb1b2434
IrinaV705 <66119487+IrinaV705@users.noreply.github.com>
2020-06-09 14:49:02 +0300

    Update main.java

--- task_2.3/main.java
+++ task_2.3/main.java
@@ -1,16 +1,17 @@
 package com.company;
-
 public class Main {
-
+    public static int multiplication(int num) {
+        int a = 1;
+        for (int i = 1; i <= num; i++) {
+            a *= i;
+        }
+        return a;
+    }
     public static void main(String[] args) {
-	// write your code here
-        long a;
-        a=1*2*3*4*5*6*7*8*9*10;
-        System.out.println(a);
+        System.out.println(multiplication(10));
     }
 }
-/*
-Вывести на экран произведение 10 чисел от 1 до 10.
+/* Вывести на экран произведение 10 чисел от 1 до 10.
 Результат - это 1 число.
 Подсказка:
 будет три миллиона с хвостиком.
@@ -18,5 +19,4 @@ public class Main {
 •	Программа должна выводить целое число на экран.
 •	Метод main должен вызывать функцию System.out.println.
 •	Выведенное число должно быть больше трех миллионов.
-•	Выведенное число должно соответствовать заданию.
-*/
+•	Выведенное число должно соответствовать заданию. */
